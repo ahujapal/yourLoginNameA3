b4c1ce847f14b4072e70ec7511bbb615ed6af175
ahujapal <50961498+ahujapal@users.noreply.github.com>
2020-07-29 05:41:17 +0530

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import { BookInfo } from "../../app/BookInfo";
+
+export const MYBOOKS: BookInfo[] = [
+    { bookName: 'Theory of Everything', author: 'Stephen Hawking', genre: 'Science', yearPublished: 2002, picture: "assets/images/theoryOfEverything.jpg"},
+    { bookName: 'War and Peace', author: 'Leo Tolstoy', genre: 'Fiction', yearPublished: 1869, picture: "assets/images/warAndPeace.jpg"},
+    { bookName: 'Devil Wears Prada', author: 'Lauren Weisberger', genre: 'Fiction', yearPublished: 2003, picture: "assets/images/devilWearsPrada.jpg"},
+    { bookName: 'Becoming', author: 'Michelle Obama', genre: 'Memoir', yearPublished: 2018, picture: "assets/images/becoming.jpg"},
+];
+    
